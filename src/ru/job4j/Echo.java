ed70549912f22d462608378a0aeffd153ef746ba
Sergei Begletsov <snower87@yandex.ru>
2021-04-17 18:32:53 +0600

    3.1. Загрузка файлов на github. [243562#271432]

---
+++ src/ru/job4j/Echo.java
@@ -0,0 +1,13 @@
+package ru.job4j;
+
+/**
+ * @Задание 3.1. Загрузка файлов на github. [243562#271432]
+ * @Описание Загрузить файл с классом "Class Echo" на gitnub
+ * @author Sergei Begletsov
+ * @since 17.04.2020
+ * @version 1
+ */
+
+public class Echo {
+
+}
