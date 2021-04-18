25a5e065e0b4bf8b0541e9efd1be88a79d024071
Sergei Begletsov <snower87@yandex.ru>
2021-04-18 14:47:01 +0600

    3.4. Заголовок в файле java. [159036#271426]

---
+++ src/ru/job4j/Info.java
@@ -0,0 +1,18 @@
+package ru.job4j;
+
+/**
+ * @Задание 3.4. Заголовок в файле java. [159036#271426]
+ * @Описание 1. Создайте класс с полным именем ru.job4j.Info.
+ * 2. Добавьте в него особый метод main с выводом сегодняшней даты.
+ * 3. Запустите метод main класса ru.job4j.Info.
+ * 4. Загрузите файл ru.job4j.Info на сайте github и оставьте ссылку на изменения.
+ * @author Sergei Begletsov
+ * @since 18.04.2020
+ * @version 1
+ */
+
+public class Info {
+    public static void main(String[] args) {
+        System.out.println("18.04.21");
+    }
+}
