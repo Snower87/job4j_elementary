eefae6c3f1a41aa1f3e0a8f67e7367d98a5a316e
Sergei Begletsov <snower87@yandex.ru>
2021-04-18 14:38:49 +0600

    3.3. Разбор первой программы. [159033#271425]

---
+++ src/ru/job4j/Multiple.java
@@ -0,0 +1,31 @@
+package ru.job4j;
+
+/**
+ * @Задание 3.3. Разбор первой программы. [159033#271425]
+ * @Описание 1. Создайте новый класс ru.job4j.Multiple.
+ * 2. Создайте в нем метод main по аналогии с классом ru.job4j.StudentInfo.
+ * 3. Добавьте в него вывод таблицы умножения на один.
+ * 1 * 2 = 2
+ * 1 * 3 = ...
+ * 1 * 9 = ..
+ * В одной строчке должен быть один пример.
+ * System.out.println("1 * 9 = 9");
+ * 4. Загрузите файл Multiple.java на сайт github и оставьте ссылку на изменения.
+ * @author Sergei Begletsov
+ * @since 18.04.2020
+ * @version 1
+ */
+
+public class Multiple {
+    public static void main(String[] args) {
+        System.out.println("1 * 1 = 1");
+        System.out.println("1 * 2 = 2");
+        System.out.println("1 * 3 = 3");
+        System.out.println("1 * 4 = 4");
+        System.out.println("1 * 5 = 5");
+        System.out.println("1 * 6 = 6");
+        System.out.println("1 * 7 = 7");
+        System.out.println("1 * 8 = 8");
+        System.out.println("1 * 9 = 9");
+    }
+}
