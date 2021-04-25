a0e2cbe1513f4f698130b01d6aad80ffef51de97
Sergei Begletsov <snower87@yandex.ru>
2021-04-25 14:59:43 +0600

    5.0.1. Сумма чисел. [226416#271460]

---
+++ src/ru/job4j/loop/Counter.java
@@ -0,0 +1,32 @@
+package ru.job4j.loop;
+
+/**
+ * @Раздел Блок 1. Базовый Синтаксис. / 4. Циклы.
+ * @Задание 5.0.1. Сумма чисел. [226416#271460]
+ * @Описание 1. Создайте класс ru.job4j.loop.Counter
+ * 2. Напишите программу, которая будет подсчитывать сумму чисел от start до finish. Например:
+ *      start = 0, finish = 5. Нужно сложить числа: 0, 1, 2, 3, 4, 5. Ответ будет 15.
+ * 3. Загрузите код в github. Оставьте ссылку на коммит.
+ * @author Sergei Begletsov
+ * @since 25.04.2020
+ * @version 1
+ */
+
+public class Counter {
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (int index = start; index <= finish; index++) {
+            sum = sum + index;
+        }
+        return sum;
+    }
+
+    public static void main(String[] args) {
+        System.out.println(sum(0, 5));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+
+        System.out.println(sum(2, 3));
+        System.out.println(sum(1, 5));
+    }
+}
