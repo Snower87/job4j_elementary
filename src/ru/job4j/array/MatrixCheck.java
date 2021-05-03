f0c887553674b0f7ecb03b912de94dbe22532add
Sergei Begletsov <snower87@yandex.ru>
2021-05-03 15:21:20 +0600

    6.7.2. Моно столбец в матрице. [214127#271392]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -2,14 +2,21 @@ package ru.job4j.array;
 
 /**
  * @Раздел Блок 1. Базовый Синтаксис. / 5. Массивы.
- * @Задание 6.7.1. Моно строка в матрице. [214126#271391]
+ * @Задание 6.7.1. Моно строка в матрице. [214126#271391] (ver. 1)
  * @Описание 1. Необходимо создать класс ru.job4j.array.MatrixCheck и реализовать метод public static boolean monoHorizontal(char[][] board, int row),
  * который проверяет заполнение строки входного двумерного массива на символ 'X'.
  * 2. Протестируйте код в junit.
  * 3. Залейте код в git-репозиторий, оставьте ссылку на коммит.
+ *
+ * @Раздел Блок 1. Базовый Синтаксис. / 5. Массивы.
+ * @Задание 6.7.2. Моно столбец в матрице. [214127#271392] (ver. 2)
+ * @Описание 1. Необходимо доработать класс ru.job4j.array.MatrixCheck и реализовать метод public static boolean monoVertical(char[][] board, int column),
+ * который проверяет заполнение столбца входного двумерного массива на символ 'X'.
+ * 2. Протестируйте код в junit.
+ * 3. Залейте код в git-репозиторий, оставьте ссылку на коммит.
  * @author Sergei Begletsov
  * @since 03.05.2020
- * @version 1
+ * @version 2
  */
 
 public class MatrixCheck {
@@ -28,6 +35,28 @@ public class MatrixCheck {
 
         for (int cols = 0; cols < columns; cols++) {
             if (board[row][cols] != 'X') {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+
+    /**
+     * Проверяет столбец входной таблицы на заполнение символом 'X'
+     * @param board ссылка на входной массив
+     * @param column столбец в коротой происходит поиск сивола 'X'
+     * @return true - если в столбце есть все символы, false - если нет символов/не полное кол-во
+     */
+    public static boolean monoVertical(char[][] board, int column) {
+        boolean result = true;
+
+        //если на входе двумерный массив, то получим его размеры
+        int rows = board.length;
+        int columns = board[0].length;
+
+        for (int row = 0; row < rows; row++) {
+            if (board[row][column] != 'X') {
                 result = false;
                 break;
             }
--- test/ru/job4j/array/MatrixCheckTest.java
+++ test/ru/job4j/array/MatrixCheckTest.java
@@ -4,6 +4,7 @@ import org.junit.Assert;
 import org.junit.Test;
 
 public class MatrixCheckTest {
+    //проверка строк
     @Test
     public void whenHasMonoHorizontal() {
         char[][] input = {
@@ -53,5 +54,45 @@ public class MatrixCheckTest {
         int row = 0;
         boolean result = MatrixCheck.monoHorizontal(input, row);
         Assert.assertFalse(result);
+    }
+
+    //проверка столбцов
+    @Test
+    public void whenHasMonoVertical() {
+        char[][] input = {
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+                {' ', ' ', 'X'},
+        };
+        int row = 2;
+        boolean result = MatrixCheck.monoVertical(input, row);
+        Assert.assertTrue(result);
+        //assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenHasMonoVerticalAtCol0() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {'X', ' ', ' '},
+                {'X', ' ', ' '},
+        };
+        int row = 0;
+        boolean result = MatrixCheck.monoVertical(input, row);
+        Assert.assertTrue(result);
+        //assertThat(result, is(true));
+    }
+
+    @Test
+    public void whenFailMonoVerticalAtCol0() {
+        char[][] input = {
+                {'X', ' ', ' '},
+                {'X', ' ', ' '},
+                {'9', ' ', ' '},
+        };
+        int row = 2;
+        boolean result = MatrixCheck.monoVertical(input, row);
+        Assert.assertFalse(result);
+        //assertThat(result, is(false));
     }
 }
