61fb6d8b042c5d6d2b8a387cb80cd38120706cbf
Sergei Begletsov <snower87@yandex.ru>
2021-07-23 09:44:51 +0500

    Устранение ошибок в п.6.4.1.1. Обход массива с последнего элемента. [#361619]

--- src/ru/job4j/array/PrintEvenElements.java
+++ src/ru/job4j/array/PrintEvenElements.java
@@ -10,14 +10,14 @@ package ru.job4j.array;
  * 3. Залейте код в репозиторий, оставьте ссылку на коммит.
  * @author Sergei Begletsov
  * @since 22.07.2021
- * @version 1
+ * @version 2
  */
 
 public class PrintEvenElements {
     public static void main(String[] args) {
         int[] numbers = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15};
         for (int i = 0; i < numbers.length; i++) {
-            if (i % 2 == 0) {
+            if ((numbers.length - 1 - i) % 2 == 0) {
                 System.out.println(
                         "Текущий элемент массива начиная с последнего: "
                                 + numbers[numbers.length - 1 - i]
